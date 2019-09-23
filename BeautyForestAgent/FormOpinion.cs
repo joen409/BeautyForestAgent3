dae747e77c7f8963bfba57dbe713d1d6233173dc
Youngseo Shin <joen50838@gmail.com>
2019-09-23 10:03:00 +0900

    #3 코드 구현 by 3507 신영서

--- BeautyForestAgent/FormOpinion.cs
+++ BeautyForestAgent/FormOpinion.cs
@@ -22,6 +22,32 @@ namespace BeautyForestAgent
             this.Close();
         }
 
-     
+        private void TsbtnFont_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.fontDlg1.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.OK:
+                    this.rtbText.SelectionFont = this.fontDlg1.Font;
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("폰트 변경을 취소하셨습니다.", "알림");
+                    break;
+            }
+        }
+
+        private void TsbtnColor_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.colorDlg.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.OK:
+                    this.rtbText.SelectionColor = this.colorDlg.Color;
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("색 변경을 취소하셨습니다.", "알림");
+                    break;
+            }
+        }
     }
 }
